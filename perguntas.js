6bbbe2370fc62df9dafc4dc7990d0eadf74d41b2
jorgestrelow <jorge.strelow@escola.pr.gov.br>
2025-05-26 10:50:28 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+  'Programação',
+  'O que é Python?',
+  'O Python é uma linguagem de progamação'
+  )
+criaCartao(
+  'Geografia'
+  Geografia',
+  'Qual a capital da França é Paris'
+)
+criaCartao(
+  'Progamação',
+  'O que é uma função?',
+  'Uma função é um bloco de código que executa alguma tarefa'
+  )
+criaCartao(
+  'Lingua inglesa',
+  'Como se diz oi em Inglês?',
+  'Oi em ingles é HI (RAI)'
+  )
